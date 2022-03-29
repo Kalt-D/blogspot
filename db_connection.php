0bf2ad0652509a259bc3c2700a4b417f29089f3c
Yan <yan.pruvot.pro@gmail.com>
2022-03-29 10:09:46 +0200

    création table html

--- db_connection.php
+++ db_connection.php
@@ -5,13 +5,6 @@ $pass = 'user1mdp';
 
 try {
     $data = new PDO('mysql:host=localhost;dbname=blogpost', $user, $pass);
-    foreach ($data->query('SELECT * FROM posts') as $row) {
-        echo $row['titre'];
-        echo "<br>";
-        echo $row['contenu'];
-        echo "<br>";
-    }
-    $data = null;
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br>";
     die();
